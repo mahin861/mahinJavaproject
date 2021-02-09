4198d52dcf9f3705a47f1e94773dfc854dbc9030
fathimah <fathima_foujul.hameem@genesys.com>
2021-02-09 19:10:09 +0530

    Addding funtion2()

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -1,13 +1,19 @@
 public class helloWorld {
     public int x = 2;
+    public static String empNo = "LK0083073";
 
     public void printVariable() {
         System.out.println("value of x:" + x);
     }
 
+    public static void funtion2(){
+        System.out.println("value of String" + "="+ empNo);
+    }
+
     public static void main(String[] arg) {
         helloWorld obj = new helloWorld();
         obj.printVariable();
+        funtion2();
 
     }
 }
